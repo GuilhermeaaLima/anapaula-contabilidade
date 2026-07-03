d312ffee4619d92e51ea519ddbf8bed0b3e5c8eb
GuilhermeaaLima <143550890+GuilhermeaaLima@users.noreply.github.com>
2026-07-03 18:29:58 -0300

    Delete site 5 escritorio contabil directory

--- site 5 escritorio contabil/script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById('navToggle');
-const navLinks = document.getElementById('navLinks');
-
-if (toggle && navLinks) {
-  toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-  });
-}
